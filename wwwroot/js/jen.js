7eae643d332fc4ce0997d22aacbafea68c5d43d0
jenalmond <jencalmond@gmail.com>
2022-01-10 09:16:13 -0700

    Add project files.

---
+++ wwwroot/js/jen.js
@@ -0,0 +1,8 @@
+﻿document.getElementById("calculate").addEventListener("click", (function () {
+    alert("Grade Calculation: " +
+        ((document.getElementById("assignment").value * .55) +
+            (document.getElementById("project").value * .05) +
+            (document.getElementById("quizzes").value * .10) +
+            (document.getElementById("exam").value * .20) +
+        (document.getElementById("intex").value * .10)));
+    }))
